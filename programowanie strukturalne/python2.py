e4e6fb91ad73ed23b18790a46d4e4f6644091e9c
PatrykHerman <patrykh94@wp.pl>
2019-03-23 17:09:12 +0100

    Pierwszy

--- programowanie strukturalne/python2.py
+++ programowanie strukturalne/python2.py
@@ -22,3 +22,48 @@ imie = ""
 zawartosc = imie.isalpha()
 print(zawartosc)
 print(type(zawartosc))
+
+imie = "Julia"
+print("\nDane uzytkownika: \nMasz na imie: " + imie)
+
+text1 = "Jan\n"
+text2 = "Nowak"
+print(text1 + text2)
+
+text = text1.rstrip()
+print(text1 + " " + text2)
+
+#wyswietlanie ciagu znakow
+#wszystkie wersje pythona
+
+text = "%s, Java i %s "%("PHP","Python")
+print(text)
+
+#nowsze wersje pythona 2.6
+text = "(0), Java i (1)".format("PHP","Python")
+print(text)
+
+#help(text.replace)
+
+new = text.replace("PHP", "C#")
+print(new)
+
+#wypisywanie danych
+rok = "2019"
+miesiac = "marzec"
+dzien = "23"
+
+print("Data: ", end="")
+print(dzien, miesiac, rok)
+
+print("Data: ", end="")
+print(dzien, miesiac, rok, sep="-")
+
+
+
+
+
+
+
+
+
